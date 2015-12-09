38346fa1c0aced69b36921f3f95d90d604d7e78e
Damon Kalaboukis <damonkala@yahoo.com>
2015-12-09 01:00:41 -0800

    Woo

--- data/schema.js
+++ data/schema.js
@@ -1,110 +1,204 @@
+"use strict";
 import {
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLInt,
-  GraphQLString,
-  GraphQLList,
-  GraphQLNonNull,
-  GraphQLID,
-  GraphQLBoolean
+	GraphQLSchema,
+	GraphQLObjectType,
+	GraphQLInt,
+	GraphQLString,
+	GraphQLList,
+	GraphQLNonNull,
+	GraphQLBoolean,
+	GraphQLID
 } from 'graphql';
 
-import {MongoClient} from "mongodb";
-let db;
+// var links = [
+// {id: 1, title: "Google", url: "https://google.com"},
+// {id: 2, title: "Yahoo", url: "yahoo.com"},
+// {id: 3, title: "HP", url: "https://hp.com"},
+// {id: 4, title: "Dell", url: "https://dell.com"},
+// {id: 5, title: "GraphQL", url: "http://graphql.org"},
+// {id: 6, title: "React", url: "http://facebook.github.io/react"},
+// {id: 7, title: "Relay", url: "http://facebook.github.io/relay"}
+// ];
+var instructors = [
+  {firstName: "George", lastName: "Someone", age: 45, gender: "male"}
+]
+var students = [
+  {fullName: "John Egbert", firstName: "John", lastName: "Egbert", age: 14, gender: "male", level:"freshman", GPA: 3.00, courses: 0},
+  {fullName: "Dave Strider", firstName: "Dave", lastName: "Strider", age: 15, gender: "male", level:"junior", GPA: 2.78, courses: 0}
+]
+var courses = [
+  {name: "math", instructor: 0}
+]
+// var grades = [
+//   {student: {fullName: "John Egbert", firstName: "John", lastName: "Egbert", age: 14, gender: "male", level:"freshman", GPA: 3.00},
+//    course: {name: "math", instructor: {firstName: "George", lastName: "Someone", age: 45, gender: "male"}},
+//     grade: "B", courses: [{name: "math", instructor: {firstName: "George", lastName: "Someone", age: 45, gender: "male"}}]},
+//
+//   {student: {fullName: "Dave Strider", firstName: "Dave", lastName: "Strider", age: 16, gender: "male", level:"freshman", GPA: 2.78},
+//    course: {name: "math", instructor: {firstName: "George", lastName: "Someone", age: 45, gender: "male"}},
+//     grade: "B", courses:[{name: "math", instructor: {firstName: "George", lastName: "Someone", age: 45, gender: "male"}}]}
+// ]
 
-MongoClient.connect(process.env.MONGO_URL, (err, database) => {
-  if (err) throw err;
+ let linkType = new GraphQLObjectType({
+	 name: 'Link',
+	 fields: () => ({
+			 id: {type: new GraphQLNonNull(GraphQLID) },
+			 title: {
+			 type: GraphQLString,
+			 args: {
+					upcase: {type: GraphQLBoolean},
+			 },
+			 resolve:
+			 (obj, {upcase}) => upcase ? obj.title.toUpperCase()  : obj.title
+		 		},
+			 url: {
+				 		 type: GraphQLString,
+			  		 resolve: (obj) => {
+							 return obj.url.startsWith("http") ? obj.url : 'http://' + obj.url
+						 }},
+				safe: {
+							type: GraphQLBoolean,
+							resolve: (obj) => {
+								return obj.url.startsWith("https")
+							}
+				}
+		 })
 
-  db = database;
-})
+	 })
+ let instructorType = new GraphQLObjectType({
+	 name: 'Instructor',
+	 fields: () => ({
+			//  id: {type: new GraphQLNonNull(GraphQLID) },
+			 firstName: { type: GraphQLString },
+       lastName: { type: GraphQLString},
+       age: { type: GraphQLInt},
+       gender: { type: GraphQLString}
+			//  resolve: () => Instructor
+		 })
 
-let linkType = new GraphQLObjectType({
-  name: 'Link',
+	 })
+ let studentType = new GraphQLObjectType({
+  name: 'Student',
   fields: () => ({
-    _id: { type: new GraphQLNonNull(GraphQLID) },
-    title: {
-      type: GraphQLString,
-      args: {
-        upcase: { type: GraphQLBoolean }
-      },
-      resolve: (obj, {upcase}) => upcase ? obj.title.toUpperCase() : obj.title
-     },
-    url: {
-      type: GraphQLString,
+       fullName: { type: GraphQLString },
+ 		   firstName: { type: GraphQLString },
+       lastName: { type: GraphQLString},
+       age: { type: GraphQLInt},
+       gender: { type: GraphQLString},
+       level: { type: GraphQLString},
+       GPA: { type: GraphQLInt},
+       courses: { type: courseType,
+        resolve: (obj)=>{
+          // console.log(obj.courses)
+          return courses[obj.courses]
+        }},
+ 	 })
+ })
+// })
+let courseType = new GraphQLObjectType({
+  name: 'Course',
+  fields: () => ({
+      name: { type: GraphQLString },
+      instructor: { type: instructorType,
       resolve: (obj) => {
-        return obj.url.startsWith("http") ? obj.url : `http://${obj.url}`
-      }
-    },
-    safe: {
-      type: GraphQLBoolean,
-      resolve: obj => obj.url.startsWith("https")
-    }
+        return instructors[obj.instructor]
+      }},
+    })
+
   })
-});
 
-
+ // let gradeType = new GraphQLObjectType({
+ //  name: 'Grade',
+ //  fields: () => ({
+ // 		//  id: {type: new GraphQLNonNull(GraphQLID) },
+ // 		 student: { type: studentType },
+ //       course: { type: courseType},
+ //       grade: {type: GraphQLString}
+ // 		//  resolve: () => Instructor
+ // 	 })
+ //
+ //  })
 let counter = 0;
 
 let schema = new GraphQLSchema({
-  // top level fields
-  query: new GraphQLObjectType({
-    name: 'Query',
-    fields: () => ({
-      counter: {
-        type: GraphQLInt,
-        resolve: () => counter
-      },
+	query: new GraphQLObjectType({
+		name: 'Query',
+		fields: () => ({
+			counter: {
+				type: GraphQLInt,
+				resolve: () => counter
+			},
+			test: {
+				type: GraphQLInt,
+				args: {
+					num: { type: GraphQLInt }
+				},
+				resolve: (_, {num}) => num * num
+			},
+			instructors: {
+				type: new GraphQLList(instructorType),
+				args: {
+					first: { type: GraphQLInt },
+				},
+				resolve: (_, {first}) => instructors.slice(0, first)
+			},
+			students: {
+				type: new GraphQLList(studentType),
+				args: {
+					filter: { type: GraphQLString },
+				},
+				resolve: (_, {first}) => students.slice(0, first)
+			},
+			courses: {
+				type: new GraphQLList(courseType),
+				args: {
+					first: { type: GraphQLInt },
+				},
+				resolve: (_, {first}) => courses.slice(0, first)
+			},
+			// grades: {
+			// 	type: new GraphQLList(gradeType),
+			// 	args: {
+			// 		first: { type: GraphQLInt },
+			// 	},
+			// 	resolve: (_, {first}) => grades.slice(0, first)
+			// },
+			allLinks: {
+				type: new GraphQLList(linkType),
+				resolve: () => links
+			},
+		})
+	}),
 
-      square: {
-        type: GraphQLInt,
-        args: {
-          num: { type: GraphQLInt }
-        },
-        resolve: (_, {num}) => num * num
-      },
+	mutation: new GraphQLObjectType({
+		name: 'Mutation',
+		fields: () => ({
+				incrementCounter: {
+					type: GraphQLInt,
+					args: {
+						delta: {type: GraphQLInt}
+					},
+					resolve: (_,{delta}) => {
+						counter = counter + delta
+						return counter;
+					}
+				},
+				createLink: {
+					type: linkType,
+					args: {
+						title: {type: GraphQLString},
+						url: {type: GraphQLString}
+					},
+					resolve:(_,{title, url}) => {
+						let newLink = {title, url, id: Date.now()};
+						links.push(newLink);
+						return newLink
+					}
+				}
+			})
+		})
 
-      links: {
-        type: new GraphQLList(linkType),
-        args: {
-          first: { type: new GraphQLNonNull(GraphQLInt) }
-        },
-        resolve: (_, {first}) => db.collection("links").find({}).limit(first).toArray()
-      },
 
-      allLinks: {
-        type: new GraphQLList(linkType),
-        resolve: () => db.collection("links").find({}).toArray()
-      },
-    })
-  }),
-
-  mutation: new GraphQLObjectType({
-    name: 'Mutation',
-    fields: () => ({
-      incrementCounter: {
-        type: GraphQLInt,
-        args: {
-          delta: {type: GraphQLInt}
-        },
-        resolve: (_, {delta}) => {
-          counter = counter + delta;
-          return counter;
-        }
-      },
-      createLink: {
-        type: linkType,
-        args: {
-          title: {type: GraphQLString},
-          url: {type: GraphQLString},
-        },
-        resolve: (_, {title, url}) => {
-          let newLink = {title, url, id: Date.now()};
-          links.push(newLink);
-          return newLink;
-        }
-      }
-    })
-  })
-});
+	});
 
 export default schema;
